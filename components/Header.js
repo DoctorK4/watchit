919a09ce995f4276b09def60c2f311b0e1853e1f
doctork4 <doctor.k1994@gmail.com>
2023-08-23 22:45:02 +0900

    헤더 로고 이미지 적용

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
 import Container from "@/components/Container";
 import styles from "./Header.module.css";
+import Logo from "/public/logo.svg"
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Container className={styles.container}>
-        <Link href="/">WATCHIT</Link>
+        <Link href="/" style={{
+          background: `url(${Logo.src}) no-repeat`,
+          width: '150px',
+        }} className={styles.a11yHidden}>
+          WATCHIT
+        </Link>
         <Link className={styles.setting} href="/setting">
           설정
         </Link>
